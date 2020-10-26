0eaea98333eb54a124ce57c5ba2c8f6b0c768a7d
David Donovan <dtdono0@msn.com>
2020-10-26 01:16:00 -0400

    Updated reference paths for hero.ts and mock-heroes.ts

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../../types/hero';
+import { HEROES } from '../../data/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
