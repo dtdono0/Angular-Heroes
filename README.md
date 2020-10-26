232ff73aaac905b4117b8bb55f290ac3e6b65824
David Donovan <dtdono0@msn.com>
2020-10-26 00:31:55 -0400

    Renamed project from AngularTest to AngularHeroes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularTest2
+# AngularHeroes
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularTest2'`, () => {
+  it(`should have as title 'AngularHeroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('AngularTest2');
+    expect(app.title).toEqual('AngularHeroes');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngularTest2!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngularHeroes!');
   });
 });
