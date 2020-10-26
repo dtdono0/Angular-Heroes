a674301cbceeb4ffed25792e37915a57ab63bdb5
David Donovan <dtdono0@msn.com>
2020-10-26 02:38:45 -0400

    Changed hero.service to return Observable data

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../types/hero';
-import { HEROES } from '../../data/mock-heroes';
 import { HeroService } from '../../services/hero.service';
 
 @Component({
@@ -24,6 +23,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from '../types/hero';
 import { HEROES } from '../data/mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,8 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 
 }
