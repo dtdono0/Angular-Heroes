f680781d0a012ec7b03ecb93f8016ef35492c2ae
David Donovan <dtdono0@msn.com>
2020-10-28 01:55:57 -0400

    Created updateHeroes method to hero.service

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -13,6 +13,8 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
 
+  httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
@@ -35,6 +37,14 @@ export class HeroService {
         tap(_ => this.log('fetched heroes')),
         catchError(this.handleError<Hero[]>('getHeroes', []))
       );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 
   /**
